4b67c8b78f50f4731de03c599a84feaa3e8ca58a
Eugene Golovanov <eugene.golovanov@hart.com>
2016-09-25 12:18:55 -0700

    Keyboard adjustment

--- ChatViewController/ChatVC.swift
+++ ChatViewController/ChatVC.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class ChatVC: UIViewController {
+class ChatVC: UIViewController, UITextFieldDelegate {
 
-    
+    @IBOutlet weak var bottomLayoutConstraint: NSLayoutConstraint!
     @IBOutlet weak var collectionViewChat: UICollectionView!
     
     let messages = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     
+    
+    //-----------------------------------------------------------------------------------
+    // MARK: - View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,10 +26,57 @@ class ChatVC: UIViewController {
         self.collectionViewChat.dataSource = self
         
     }
+    
+    //-----------------------------------------------------------------------------------
+    // MARK: - keyboard
+
+    
+    func keyboardWillShow(notification: NSNotification) {
+        self.adjustInsetForKeyboard(show: true, notification: notification)
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        self.adjustInsetForKeyboard(show: false, notification: notification)
+    }
+    
+    func adjustInsetForKeyboard(show:Bool, notification: NSNotification) {
+        let userinfo = notification.userInfo ?? [:]
+        let keyboardFrame = (userinfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        let adjustmentHeight = keyboardFrame.height * (show ? 1 : 0)
+        
+        bottomLayoutConstraint.constant = adjustmentHeight
+        self.view.layoutIfNeeded()
+//        self.collectionViewChat.layoutIfNeeded()
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
+
+    
+    //-----------------------------------------------------------------------------------
+    // MARK: - init deinit
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.keyboardWillShow(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.keyboardWillHide(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    deinit {
+        
+    }
+
 
 
 
 }
+
+
 
 
 extension ChatVC: UICollectionViewDelegate, UICollectionViewDataSource {
